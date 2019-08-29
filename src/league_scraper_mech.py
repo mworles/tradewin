424fc2e59b6d62c45cc0e613a6487a97ace60f52
Matthew Worley <mworles@gmail.com>
2019-08-29 12:31:27 -0700

    mods to league_scraper_mech.py

--- src/league_scraper_mech.py
+++ src/league_scraper_mech.py
@@ -20,6 +20,16 @@ import mechanize
 import cookielib
 import html2text
 
+import sys, logging
+logger = logging.getLogger("mechanize.cookies")
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.DEBUG)
+
+def print_link_text(links):
+    for link in links:
+        print link.text
+
+
 # Browser
 br = mechanize.Browser()
 
@@ -44,17 +54,44 @@ sign_in = br.find_link(text="Sign in")
 br.follow_link(sign_in)
 
 # View available forms
-
+#for f in br.forms():
+    #print f
 br.select_form(nr=0)
-br.form['username'] = 'mworles40'
-br.form['passwd'] = 'kentucky2@'
-br.submit(id='login-signin')
 
+br.form['username'] = 'mworles40'
+#br.click(name='signin')
+br.form['passwd'] = 'kentucky2@'
+br.set_simple_cookie('AO', 'u=1', '.yahoo.com', path='/')
+br.set_simple_cookie('B',
+                          '23mqeupemg7ej&b=4&d=eOkh961pYEJITp9VsPWCfZTpLTkzRjTm1GwaDg--&s=q2&i=IQaBIZc6MWVhDMLrsBz.',
+                          '.yahoo.com', path='/')
+br.set_simple_cookie('F',
+                          'd=0U_gff89vJx2ETiwBsqG5J6jBiHzMbCAmABBmoQ-',
+                          '.yahoo.com', path='/')
+br.set_simple_cookie('GUC', 'AQEAAQJdaW1eNEIgTQTN&s=AQAAAPt3gggq&g=XWgd8w',
+                          '.yahoo.com', path='/')
+br.set_simple_cookie('GUCS', 'ARASEppE', '.yahoo.com', path='/')
+br.set_simple_cookie('PH', 'fn=E0PIEOCed89JkOVJ.0TJcg--&l=en-US&i=us', '.yahoo.com', path='/')
+br.set_simple_cookie('T', 'z=p3BadBpLpedB6f6zLKknHKGNDU0MQY1MzczNzVPNjc2&a=QAE&sk=DAArDLCXtDyzlu&ks=EAA9tevPxpsUQajZyo.NMl2Pw--~G&kt=EAAtwBx8ExCNgQWheBrUp8Ifw--~I&ku=FAAjGqWGll.0eYRqBZA7KCM6RalnmfMvUcZYtb4mJbGnO3PLpzgJxRze9QAfONk8xpVIcZh9Y0XC2GbmxocvTBAtY6IV79fc0OfJovYSZrI6WLKA.WNtiO07v1peoUjF83Vy3z5RTB.SZ1HMbvsKiy5X4EZZbJqS1uMCxf6AsV3TKk-~A&d=bnMBeWFob28BZwFWVldXTU4zWE81SUQ1RU1XVkNDQVhGQVA1UQFzbAFNekl6TmdFeU5EQTBNREk0TVRBeAFhAVFBRQFhYwFBRW83QURkbAFsYXQBcDNCYWRCAWNzAQFzYwFkZXNrdG9wX3dlYgFmcwFfcVRPbE5sZGFCM3ABenoBcDNCYWRCQTdF&af=JnRzPTE1NjcxMDQ0ODkmcHM9WE96Uk1zTmo5angzQWZKdUpsZTd0US0t', '.yahoo.com', path='/')
+br.set_simple_cookie('Y', 'v=1&n=713qmr9teevs4&l=cmehb4iuq/o&p=m2j0iln00000000&r=f7&intl=us', '.yahoo.com', path='/')
+
+
+#br.set_cookie("t=1567099041&j=0; expires=Friday, 20-Aug-20 23:12:40 GMT")
+#for control in br.form.controls:
+    #print control
+    #print "type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+response = br.submit()
+print response.read()
+"""
+#click_response = br.click(name='signin')
+#response = mechanize.urlopen(click_response)
+#print(response.read())
+
+#br.submit(name='signin')
+#br.submit(id='login-signin')
 #url_base = "https://football.fantasysports.yahoo.com/f1/"
 #url_team= "".join([url_base, str(league[2]['LEAGUE_ID']), '/', str(1)])
 
 url = "https://football.fantasysports.yahoo.com/f1/1009392"
 br.open(url_team)
-
-for link in br.links():
-    print link.text
+"""
