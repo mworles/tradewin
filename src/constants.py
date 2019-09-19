5ece07ece03834737e027e8844ae822a69d032b0
Matthew Worley <mworles@gmail.com>
2019-09-19 12:12:13 -0700

    add scoring settings to constants

--- src/constants.py
+++ src/constants.py
@@ -8,3 +8,16 @@ POSITIONS = ['QB', 'RB', 'WR', 'TE', 'FLEX']
 POSITIONS_NOFLEX = ['QB', 'RB', 'WR', 'TE']
 
 FLEX_POSITIONS = ['RB', 'WR', 'TE']
+
+SETTINGS = {'pss_cmp': 0.25,
+            'pss_inc': -0.25,
+            'pss_yds': 0.04,
+            'pss_td': 5,
+            'pss_int': -2,
+            'rsh_yds': 0.10,
+            'rsh_td': 6,
+            'rec_rec': 0.5,
+            'rec_yds': 0.10,
+            'rec_td': 6,
+            'fmbl': 2,
+            'rsh_att': 0}
