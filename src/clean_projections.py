001a8e72d52c3d8fa3e32b8ab955ba944fe1dc7f
Matthew Worley <mworles@gmail.com>
2019-09-25 09:54:53 -0700

    create modular function to use for different league settings

--- src/clean_projections.py
+++ src/clean_projections.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 import sys
-from constants import score_dict
+from League import LEAGUE
 
-def score_row(row, cols, score_dict):
+def score_row(row, cols, lid=1):
+    score_dict = LEAGUE[lid]['SETTINGS']
     pts = 0
     for c in cols:
         try:
@@ -14,32 +15,38 @@ def score_row(row, cols, score_dict):
     pts = round(pts, 1)
     return pts
 
-# set base data directory
-data_in = "../data/"
+def score_projections(lid=1):
+    
+    score_dict = LEAGUE[lid]['SETTINGS']
+    
+    # set base data directory
+    data_in = "../data/"
+    
+    # import clean projections data
+    df = pd.read_csv(data_in + 'nf_projections.csv')
 
-# import clean projections data
-df = pd.read_csv(data_in + 'nf_projections.csv')
+    df['pss_cmp'] = df['cmp/att'].apply(lambda x: float(x.split('/')[0]))
+    df['pss_att'] = df['cmp/att'].apply(lambda x: float(x.split('/')[1]))
+    df['pss_inc'] = df['pss_att'] - df['pss_cmp'] 
 
-df['pss_cmp'] = df['cmp/att'].apply(lambda x: float(x.split('/')[0]))
-df['pss_att'] = df['cmp/att'].apply(lambda x: float(x.split('/')[1]))
-df['pss_inc'] = df['pss_att'] - df['pss_cmp'] 
+    keep_cols = ['name', 'pos', 'team']
+    stat_cols = score_dict.keys()
+    stat_cols = [x for x in score_dict.keys() if x in df.columns]
+    keep_cols.extend(stat_cols)
 
-keep_cols = ['name', 'pos', 'team']
-stat_cols = score_dict.keys()
-stat_cols = [x for x in score_dict.keys() if x in df.columns]
-keep_cols.extend(stat_cols)
+    cols = list(df.columns)
 
-cols = list(df.columns)
+    print "scoring projections"
+    df['fpts'] = df.apply(lambda x: score_row(x, cols), axis=1)
+    keep = ['name', 'team', 'pos', 'fpts']
+    df = df[keep]
 
-print "scoring projections"
-df['fpts'] = df.apply(lambda x: score_row(x, cols, score_dict), axis=1)
-keep = ['name', 'team', 'pos', 'fpts']
-df = df[keep]
+    df['pos'] = df['pos'].str.lower()
 
-df['pos'] = df['pos'].str.lower()
+    df = df.sort_values('fpts', ascending=False)
 
-df = df.sort_values('fpts', ascending=False)
-
-file_name = data_in + 'projections_scored.csv'
-print "writing %s" % (file_name)
-df.to_csv(file_name, index=False)
+    lg_sub = 'league_' + str(lid) + '/'
+    
+    file_name = data_in + lg_sub + 'projections_scored.csv'
+    print "writing %s" % (file_name)
+    df.to_csv(file_name, index=False)
