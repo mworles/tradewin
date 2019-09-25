f42cc6996b520aa00c0c102c3a42574c86d02d8f
Matthew Worley <mworles@gmail.com>
2019-09-25 09:55:48 -0700

    remove trade result printing, to use inspect_trades.py instead

--- src/run.py
+++ src/run.py
@@ -12,29 +12,32 @@ def get_dt():
     dt = date + time
     return dt
 
+lid=2
+lgdir = 'league_' + str(lid)
 NF_CSV = '../data/nf_projections.csv'
-ROSTER_CSV = '../data/rosters.csv'
-YAHOO_CSV = '../data/yahoo_projections.csv'
+ROSTER_CSV = "".join(['../data/', lgdir, '/rosters.csv'])
+YAHOO_CSV = "".join(['../data/', lgdir, '/yahoo_projections.csv'])
 
-rosters = update_rosters(NF_CSV, YAHOO_CSV, ROSTER_CSV)
+rosters = update_rosters(NF_CSV, YAHOO_CSV, ROSTER_CSV, lid=lid)
+
 free_agents = update_free_agents(NF_CSV, YAHOO_CSV, ROSTER_CSV)
 tg = TradeGenerator(rosters, free_agents)
-tgt_teams = [9] #[t.team_number for t in rosters]
-#tgt_teams.remove(3)
+tgt_teams = [5, 6, 8] #[t.team_number for t in rosters if t.team_number != MY_TEAM]
 my_max = 3
 other_max = 2
-to_get = ['Mike Evans', 'Lamar Jackson']
+to_get = [] #['Dalvin Cook']
+to_give = [] #['Travis Kelce'] #, 'Leonard Fournette'] 
+my_bl = [] #['Curtis Samuel', 'Kyler Murray']
+other_bl = ['Damien Williams']
 
 trades = tg.get_trades(teams = tgt_teams,
                        my_max = my_max,
-                       other_max = other_max)
-
-trades = [t for t in trades if t.my_nf_gain > 5 and t.other_yh_gain > 5]
-sortfunc = lambda x: x.my_nf_gain #lambda x: x.my_nf_gain + x.other_yh_gain
-trades.sort(key=sortfunc) #, reverse=True)
-
-for t in trades[-50:]:
-    t.print_trade()
+                       other_max = other_max) #,
+                       to_get = to_get,
+                       to_give = to_give,
+                       my_bl = my_bl, 
+                       other_bl = other_bl,
+                       fa = False)
 
 pik = "../trades/trade_run_" + get_dt() +  ".dat"
 pik_now = "../trades/trade_run.dat"
@@ -43,3 +46,15 @@ with open(pik, "wb") as f:
     pickle.dump(trades, f)
 
 shutil.copy(pik, pik_now)
+"""
+all_players = []
+
+for r in rosters:
+    all_players.extend(r.team_players)
+    
+sortfunc = lambda x: x.nf_yh
+all_players.sort(key=sortfunc)
+
+for p in all_players[-50:]:
+    print "  ".join([p.name, str(p.nf_projection), str(p.yh_projection)])
+"""
